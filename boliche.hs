0ba6fb17655f0914a09cd38fe3b64c26eaf9512b
Juan Cuiule <juanignaciocuiule@gmail.com>
2017-04-16 17:37:07 -0300

    fix en show

--- boliche.hs
+++ boliche.hs
@@ -1,23 +1,31 @@
+type Nombre = String
+type Resistencia = Int
+
 -- Punto 1
 data Cliente = UnCliente {
-	nombre :: String,
-	resistencia :: Int,
+	nombre :: Nombre,
+	resistencia :: Resistencia,
 	amigos :: [Cliente]
 }
 
 -- Punto 2
-rodri = UnCliente "Rodri" 55 []
+rodri = UnCliente "Rodri" 55 [marcos]
 marcos = UnCliente "Marcos" 40 [rodri]
 cristian = UnCliente "Cristian" 2 []
 ana = UnCliente "Ana" 120 [rodri, marcos]
 
-listaNombres :: [Cliente] -> [String]
-listaNombres listaDeAmigos =
+-- funciones para usar en show Cliente
+amigoATupla :: Cliente -> (Nombre, Resistencia, [String])
+amigoATupla cliente =
+	(nombre cliente, resistencia cliente, (armarListaDeNombres.amigos) cliente)
+
+armarListaDeNombres :: [Cliente] -> [String]
+armarListaDeNombres listaDeAmigos =
 	map nombre listaDeAmigos
 
 instance Show Cliente where
   show (UnCliente nombre resistencia amigos) =
-  	"Nombre: " ++ show nombre ++ "\nResistencia: " ++ show resistencia ++ "\nAmigos: " ++ show (listaNombres amigos)
+  	"Nombre: " ++ show nombre ++ ", Resistencia: " ++ show resistencia ++ ", Amigos: " ++ show (map amigoATupla amigos)
 
 -- Punto 3
 comoEsta :: Cliente -> String
@@ -45,14 +53,14 @@ type Tiempo = Int
 
 grogXD :: Bebida
 grogXD cliente =
-	UnCliente
-		(nombre cliente)
-		0
-		(amigos cliente)
+	UnCliente (nombre cliente) 0 (amigos cliente)
 
-jarraLoca :: Bebida
+jarraLoca :: Cliente -> Cliente
 jarraLoca cliente =
-	UnCliente (nombre cliente) (resistencia cliente - 10) (amigos cliente)
+	UnCliente
+			(nombre cliente)
+			(resistencia cliente - 10)
+			(map jarraLoca (amigos cliente))
 
 klusener :: Gusto -> Bebida
 klusener gusto cliente =
@@ -75,8 +83,14 @@ soda fuerza cliente =
 	(resistencia cliente)
 	(amigos cliente)
 
+-- Punto 6
 rescatarse :: Tiempo -> Cliente -> Cliente
 rescatarse tiempo cliente
 	| tiempo < 0 = cliente
 	| tiempo > 3 = UnCliente (nombre cliente) (resistencia cliente + 200) (amigos cliente)
 	| otherwise = UnCliente (nombre cliente) (resistencia cliente + 100) (amigos cliente)
+
+-- Punto 7
+-- Consulta a la consola
+-- *Main> ((klusener "huevo").(rescatarse 2).(klusener "chocolate").jarraLoca) ana
+-- Nombre: "Ana", Resistencia: 196, Amigos: [("Rodri",45,["Marcos"]),("Marcos",30,["Rodri"])]
