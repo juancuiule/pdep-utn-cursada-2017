a26a853c77cd5f9763285fc43fb75862cb76838c
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2017-06-09 00:51:49 -0300

    base de conocimiento tp lógico

---
+++ Logico/tp-logico.pl
@@ -0,0 +1,8 @@
+% pareja(Persona, Persona)
+pareja(marsellus, mia).
+pareja(pumkin, honeyBunny).
+
+% trabajaPara(Empleadeor, Empleado).
+trabajaPara(marsellus, vincent).
+trabajaPara(marsellus, jules).
+trabajaPara(marsellus, winston).
