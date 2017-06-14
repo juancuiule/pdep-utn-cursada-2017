fe483575b92b5960a55bee00e2f6731174ef6ead
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2017-06-14 19:24:32 -0300

    base de conocimiento segunda entrega

--- Logico/tp-logico.pl
+++ Logico/tp-logico.pl
@@ -41,3 +41,32 @@ acataOrden(Empleador, Empleado) :-
 acataOrden(Empleador, Empleado) :-
 	trabajaPara(Empleador, Otro),
 	acataOrden(Otro, Empleado).
+
+% personaje(Nombre, Ocupacion)
+personaje(pumkin,     ladron([estacionesDeServicio, licorerias])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent,    mafioso(maton)).
+personaje(jules,      mafioso(maton)).
+personaje(marsellus,  mafioso(capo)).
+personaje(winston,    mafioso(resuelveProblemas)).
+personaje(mia,        actriz([foxForceFive])).
+personaje(butch,      boxeador).
+personaje(bernardo,   mafioso(cerebro)).
+personaje(bianca,     actriz([elPadrino1])).
+personaje(elVendedor, vender([humo, iphone])).
+personaje(jimmie,     vender([auto])).
+
+% encargo(Solicitante, Encargado, Tarea). 
+% las tareas pueden ser cuidar(Protegido), ayudar(Ayudado), buscar(Buscado, Lugar)
+encargo(marsellus, vincent,   cuidar(mia)).
+encargo(vincent,  elVendedor, cuidar(mia)).
+encargo(marsellus, winston, ayudar(jules)).
+encargo(marsellus, winston, ayudar(vincent)).
+encargo(marsellus, vincent, buscar(butch, losAngeles)).
+encargo(bernardo, vincent, buscar(jules, fuerteApache)).
+encargo(bernardo, winston, buscar(jules, sanMartin)).
+encargo(bernardo, winston, buscar(jules, lugano)).
+
+amigo(vincent, jules).
+amigo(jules, jimmie).
+amigo(vincent, elVendedor).
